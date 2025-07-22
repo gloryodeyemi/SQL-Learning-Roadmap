0a432faa1ac3f16669ac086690b5b5c3ccc7653a
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-07-21 20:57:18 -0400

    AND Operator

--- Quiz/data_cleaning_quiz.sql
+++ Quiz/data_cleaning_quiz.sql
@@ -2,15 +2,28 @@
 -- 1. True or False: The DISTINCT keyword in SQL is used to eliminate duplicate values from your query results. Explain your reasoning.
 -- The DISTINCT keyword does indeed eliminate duplicate values, ensuring that only unique entries are returned in the result set.
 
--- Imagine you have a table named products with a column named category. Write a SQL query that returns a list of all unique categories in the 
+-- 2. Imagine you have a table named products with a column named category. Write a SQL query that returns a list of all unique categories in the 
 -- products table.
 SELECT DISTINCT category
 FROM products;
 
--- You have a table named sales with columns product_id, customer_id, and sale_date. You want to find out how many unique customers purchased each 
--- product. How would you use DISTINCT in combination with COUNT and GROUP BY to achieve this? Explain your approach.
+-- 3. You have a table named sales with columns product_id, customer_id, and sale_date. You want to find out how many unique customers purchased  
+-- each product. How would you use DISTINCT in combination with COUNT and GROUP BY to achieve this? Explain your approach.
 SELECT product_id, COUNT(DISTINCT customer_id) AS unique_customers
 FROM sales
 GROUP BY product_id;
 
 -- AND Operator
+-- 1. True or False: The SQL AND operator requires all conditions to be TRUE for a row to be included in the result. Explain your reasoning.
+-- The AND operator acts as a filter, ensuring that only rows satisfying all specified conditions are included in the result set. If even one 
+-- condition is FALSE, the entire expression evaluates to FALSE, and the row is excluded.
+
+-- 2. Imagine you have a table named products with columns price and in_stock. Write a SQL snippet that selects all products that cost more than 
+-- 20 dollars and are in stock.
+SELECT *
+FROM products
+WHERE price > 20 AND in_stock = TRUE;
+
+-- 3. How does the AND operator handle NULL values in SQL? Explain what happens when you combine TRUE AND NULL and FALSE AND NULL.
+-- When the AND operator encounters a TRUE and NULL, the result is NULL and when it encounters FALSE and NULL, the result is FALSE. Only rows 
+-- that are TRUE are returned.

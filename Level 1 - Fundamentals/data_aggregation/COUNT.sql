d538491fcae2c4d82412ed17fbe76387c93f7ce7
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-08-13 15:40:34 -0400

    SQL COUNT Function

---
+++ Level 1 - Fundamentals/data_aggregation/COUNT.sql
@@ -0,0 +1,5 @@
+/*
+SQL COUNT Function
+
+It does exactly what its name suggests: it counts things. Whether you need to count the total number of users, the number of videos per creator, the quantity of non-null entries in a column, or the variety of interaction types in your database, COUNT is your go-to tool.
+*/
